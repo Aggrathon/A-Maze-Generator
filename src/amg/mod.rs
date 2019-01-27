0ac98939014821818ce50b64930f9cfbb861c3de
Aggrathon <antonbjo@gmail.com>
2019-01-28 01:45:39 +0200

    walls and generation loop

--- src/amg/mod.rs
+++ src/amg/mod.rs
@@ -1,3 +1,7 @@
+
+use rand::thread_rng;
+use rand::seq::SliceRandom;
+
 mod structures;
 
 pub struct Maze {
@@ -10,6 +14,14 @@ pub struct Maze {
 impl Maze {
     pub fn new(width: usize, height: usize) -> Maze {
         let mut maze: Vec<i32> = vec![0; width*height];
+        for i in 0..width {
+            maze[i] = -1;
+            maze[width * height - i - 1] = -1;
+        }
+        for i in 1..(height-1) {
+            maze[i*width] = -1;
+            maze[(i+1)*width - 1] = -1;
+        }
         let x = width / 2;
         maze[x] = 1;
         maze[width*height - x - 1] = 2;
@@ -17,12 +29,34 @@ impl Maze {
     }
 
     pub fn generate(&mut self) {
+        let mut rnd = thread_rng();
         let str_size = 3;
-        let str_cnt = (self.width * self.height) / (str_size * str_size * 4);
-        structures::add_structures(self, str_cnt, str_size, str_size, 3);
-        // loop { //TODO: Check for stopping
-        //     wilson(self, 0, 0);
-        // }
+        let str_cnt = (self.width * self.height) / (str_size * str_size * 2);
+        let doors: Vec<usize> = structures::add_structures(self, str_cnt, str_size, str_size, 3, 4);
+        for i in doors.iter() {
+            let x = *i%self.width;
+            let y = *i/self.width;
+            wilson(self, x, y);
+        }
+        loop {
+            let mut tiles: Vec<usize> = (0..(self.width*self.height)).filter(|x| self.maze[*x] == 0).collect();
+            tiles.shuffle(&mut rnd);
+            for i in tiles.iter() {
+                let neigh: Vec<i32> = vec![i-1, i+1, i-self.width, i+self.width].into_iter().map(|x| self.maze[x]).filter(|x| *x > 0).collect();
+                if neigh.len() == 0 {
+                    let x = *i%self.width;
+                    let y = *i/self.width;
+                    wilson(self, x, y);
+                } else if neigh.len() > 1 && neigh.iter().any(|x| *x != neigh[1]) {
+                    let x = *i%self.width;
+                    let y = *i/self.width;
+                    kruskal(self, x, y);
+                }
+            }
+            if doors.iter().all(|x| self.maze[*x] == 1) {
+                break;
+            }
+        }
     }
 
     pub fn get(&self, x:usize, y:usize) -> i32 {
@@ -37,8 +71,12 @@ impl Maze {
         for i in (0..self.height).rev() {
             for j in 0..self.width {
                 let tile = self.maze[j + i * self.width];
-                if tile <= 0 {
-                    print!("# ");
+                if tile == 0 {
+                    print!("##");
+                } else if tile < 0 {
+                    print!("@@");
+                } else if tile == 1 {
+                    print!("  ");
                 } else {
                     print!("{} ", tile);
                 }
@@ -59,6 +97,10 @@ pub fn wilson(maze: &mut Maze, x: usize, y: usize) {
         // Update maze
         return;
     }
+}
+
+pub fn kruskal(mase: &mut Maze, x: usize, y: usize) {
+
 }
 
 
--- src/amg/structures.rs
+++ src/amg/structures.rs
@@ -1,5 +1,5 @@
 use super::Maze;
-use rand::Rng;
+use rand::{Rng, seq::IteratorRandom};
 
 struct Rect(usize, usize, usize, usize);
 
@@ -7,12 +7,22 @@ impl Rect {
     fn overlaps(&self, other: &Rect) -> bool {
         !(self.2 < other.0 || self.0 > other.2 || self.3 < other.1 || self.1 > other.3)
     }
+
+    fn border(&self, width: usize) -> Vec<usize> {
+        let t = (self.0 + (self.1 - 1) * width)..(self.2 + (self.1 - 1) * width);
+        let b = (self.0 + (self.3) * width)..(self.2 + (self.3) * width);
+        let l = (self.1..self.3).map(|x| x * width + self.0 - 1);
+        let r = (self.1..self.3).map(|x| x * width + self.2);
+        t.chain(b).chain(l).chain(r).collect()
+    }
 }
 
-pub fn add_structures(maze: &mut Maze, count: usize, max_width: usize, max_height: usize, wall_offset: usize) {
+pub fn add_structures(maze: &mut Maze, count: usize, max_width: usize, max_height: usize, wall_offset: usize, max_doors: usize) -> Vec<usize> {
     let mut rnd = rand::thread_rng();
     let mut struc: Vec<Rect> = Vec::new();
+    let mut doors: Vec<usize> = Vec::new();
     'outer: for i in 0..count {
+        // Generate Room
         let w = rnd.gen_range(2, max_width+1);
         let h = rnd.gen_range(2, max_height+1);
         if w + wall_offset*2 >= maze.width || h + wall_offset*2 >= maze.height { continue; }
@@ -20,15 +30,30 @@ pub fn add_structures(maze: &mut Maze, count: usize, max_width: usize, max_heigh
         let y = rnd.gen_range(wall_offset, maze.height - h - wall_offset - 1);
         let r = Rect(x, y, x + w, y + h);
         let r2 = Rect(x-wall_offset, y-wall_offset, x + w + wall_offset, y + h + wall_offset);
+        // Check Collisions
         for r3 in struc.iter() { if r2.overlaps(&r3) { continue 'outer; }}
-        struc.push(r);
-        for k in y..(y+h) {
-            for j in x..(x+w) {
+        // Create Room
+        for k in y..(y + h) {
+            for j in x..(x + w) {
                 maze.maze[j + k * maze.width] = maze.counter;
             }
         }
+        // Create Walls
+        let wall: Vec<usize> = r.border(maze.width);
+        for j in wall.iter() {
+            maze.maze[*j] = -1;
+        }
+        // Create Doors
+        let nd = rnd.gen_range(2, max_doors);
+        let door: Vec<usize> = wall.into_iter().choose_multiple(&mut rnd, nd);
+        for j in door.iter() {
+            maze.maze[*j] = maze.counter;
+        }
+        doors.extend(door);
+        struc.push(r);
         maze.counter = maze.counter + 1
     }
+    doors
 }
 
 #[cfg(test)]
