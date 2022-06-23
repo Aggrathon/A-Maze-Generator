432954c2c09b230138a9799d84c35bbdbbb747b8
Aggrathon <antonbjo@gmail.com>
2022-06-23 22:17:38 +0300

    fix outdated dependency

--- src/amg/image.rs
+++ src/amg/image.rs
@@ -1,26 +1,35 @@
-
-use image::{GrayImage, Luma, FilterType, imageops::resize, Rgb, RgbImage, ConvertBuffer};
 use super::Maze;
+use image::{
+    buffer::ConvertBuffer,
+    imageops::{resize, FilterType},
+    GrayImage, Luma, RgbImage,
+};
 
 impl Maze {
     pub fn to_image(&self, tile_width: u32) -> GrayImage {
-        let mut img = GrayImage::from_fn(self.width as u32, self.height as u32, |x, y| {
-            match self.get(x as usize, y as usize) {
-                d if d < 0 => Luma([0u8]),
-                0          => Luma([32u8]),
-                d if d > 0 => Luma([255u8]),
-                _          => Luma([128u8])
-            }
+        let mut img = GrayImage::from_fn(self.width as u32, self.height as u32, |x, y| match self
+            .get(x as usize, y as usize)
+        {
+            d if d < 0 => Luma([0u8]),
+            0 => Luma([32u8]),
+            d if d > 0 => Luma([255u8]),
+            _ => Luma([128u8]),
         });
-        self.structures.iter().for_each(|i| i.for_each(|(x, y)| img.put_pixel(x as u32, y as u32, Luma([192u8]))));
-        resize(&img, img.width() * tile_width, img.height() * tile_width, FilterType::Nearest)
+        self.structures
+            .iter()
+            .for_each(|i| i.for_each(|(x, y)| img.put_pixel(x as u32, y as u32, Luma([192u8]))));
+        resize(
+            &img,
+            img.width() * tile_width,
+            img.height() * tile_width,
+            FilterType::Nearest,
+        )
     }
 
     pub fn to_image_color(&self, tile_width: u32) -> RgbImage {
         self.to_image(tile_width).convert() as RgbImage
     }
 }
-
 
 #[cfg(test)]
 mod tests {
